cbcabb42e2727e575b4004c9f2af8a8ff7659d81
mrtoxicbotgmail9570-oss <mrtoxicbotgmail9570@gmail.com>
2026-07-04 23:51:52 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -7,14 +7,14 @@ API_ID = int(environ.get("API_ID", "38498066"))
 API_HASH = environ.get("API_HASH", "c9696114751feacdeb1b4487f5839a1a")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "8446475678"))
-CREDIT = environ.get("CREDIT", " @JapaneseFury")
+OWNER = int(environ.get("OWNER", "8902042822"))
+CREDIT = environ.get("CREDIT", " @MR_Toxic_1")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
 TOTAL_USER = os.environ.get('TOTAL_USERS', '8723278238,8480660521,7988815969,6660248311,8680968748,8745263057,8703802029').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '8723278238,8480660521,7988815969,6660248311,8680968748,8745263057,8703802029').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '8723278238,8902042822,8480660521,7988815969,6660248311,8680968748,8745263057,8703802029').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
